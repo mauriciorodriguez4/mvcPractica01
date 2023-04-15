7cda84c54c87f450937205d9c13c25da54a2548b
maurirod <mauricio.rodriguez4@catolica.edu.sv>
2023-04-15 09:29:49 -0600

    Creación de la vista Equipos

---
+++ mvcPractica01/Controllers/equiposController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvcPractica01.Controllers
+{
+    public class equiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
